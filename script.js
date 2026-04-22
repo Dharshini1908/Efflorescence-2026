02b4716877a7dad5a9f1a3ae2ac8d316c600c513
Dharshini1908 <dharshini5240177@gmail.com>
2026-04-22 19:48:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-function openEvent(event) {
+window.onload = function(){
 
-    let details = {
-        paper: "Paper Presentation\n\nRules:\n- Max 2 members\n- 5 mins time",
-        quiz: "Technical Quiz\n\nRules:\n- Individual or team",
-        hack: "Hack & Hunt\n\nSolve clues and code",
-        mini: "Mini Hackathon\n\nBuild project in limited time",
-        code: "Crack the Code\n\nDebug and solve",
-        dance: "Dance Event\n\nSolo or group",
-        sing: "Singing Event",
-        beat: "Chase the Beat",
-        puzzle: "Solve the Puzzle",
-        hunt: "Pressure Hunt"
-    };
+  const cards = document.querySelectorAll(".card");
 
-    alert(details[event]);
+  function showCards(){
+    const trigger = window.innerHeight * 0.85;
+
+    cards.forEach(card => {
+      const top = card.getBoundingClientRect().top;
+
+      if(top < trigger){
+        card.classList.add("show");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", showCards);
+  showCards();
 }
-var eventDate = new Date("April 29, 2026 09:00:00").getTime();
-
-var x = setInterval(function () {
-
-    var now = new Date().getTime();
-    var distance = eventDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
-
-    document.getElementById("countdown").innerHTML =
-        "⏳ " + days + " Days " + hours + " Hours Left";
-
-}, 1000);
